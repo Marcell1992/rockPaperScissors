8ced3bcac9c4613ff533f74919d7077325b43f8c
Marcell Gannon <gannon.marcell@hotmail.com>
2023-09-30 20:01:31 +0200

    Implement the game fully as outlined in the assignment
    *Implemented score tracker
    
    *Implemented game out of 5(No loop unfortunately...for now)
    
    Bug:Very first round, console doesn't keep proper track

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
     <li>Random number is generated and compared, outputting a choice of rock, paper or scissors for the 'computer'</li>
     <li>Player can input text of rock, paper or scissors</li>
     <li>Values of the 'computer' and the player are compared and the winner determined</li>
+    <li>Overall winner of 5 rounds is determined and ouputted</li><br>
 <p>Project will be expanded upon as The Odin Project's course continues to implement GUI</p>
--- script.js
+++ script.js
@@ -1,39 +1,73 @@
-/* rock, paper, scissors, lizard, spock
+/* rock, paper, scissors, lizard, spock:
+scissors cuts paper
+paper covers rock
 rock crushes lizard
 lizard poisons spock
 spock smashes scissors
-scissors cuts lizard
+scissors decapitates lizard
 lizard eats paper
 paper disproves spock
 spock vaporizes rock
+and as it always has, rock crushes scissors
 */
 
-function getComputerChoice(randomNumber){
-    if(randomNumber == 1){
-      return answer = 'Paper';
-    }else if (randomNumber == 2){
-      return answer = 'Rock';
-    }else{
-      return answer = 'Scissor';
+let playerScore = 0;
+let computerScore = 0;
+
+function game(){
+    let randomNumber = Math.floor(Math.random() *3)+1;
+    const playerSelection = prompt("Rock, paper or scissors?",'');
+    const computerSelection = getComputerChoice(randomNumber);
+    console.log(`Computer's choice is: ${computerSelection}`);
+    playRound(playerSelection, computerSelection);
+    console.log(`Your score is: ${playerScore}. The computer's score is: ${computerScore}`);
+    console.log('');
+
+    function getComputerChoice(randomNumber){
+        if(randomNumber == 1){
+          return 'Paper';
+        }else if (randomNumber == 2){
+          return 'Rock';
+        }else{
+          return 'Scissors';
+        }
+    }
+
+    function playRound(playerSelection, computerSelection){
+        if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
+            console.log("That's a draw. No score for either");
+
+        }else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper' ||
+                  playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors' ||
+                  playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock'){
+            
+            computerScore += 1;
+            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+            
+        }else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'scissors' ||
+                  playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'rock' ||
+                  playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'paper'){
+            
+            playerScore += 1;
+            console.log(`You win! ${playerSelection} beats ${computerSelection}`);
+
+        }else{
+            alert("This is a game of rock, paper, scissors");
+            console.log('');
+            game();
+        }
     }
 }
-let randomNumber = Math.floor(Math.random() *3)+1;
-const playerSelection = prompt("Rock, paper or scissors?",'');
-const computerSelection = getComputerChoice(randomNumber);
-console.log(computerSelection);
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()){
-        console.log('Round draw');
-        
-    }else if (playerSelection.toLowerCase() == 'rock' && computerSelection.toLowerCase() == 'paper' ||
-              playerSelection.toLowerCase() == 'paper' && computerSelection.toLowerCase() == 'scissors' ||
-              playerSelection.toLowerCase() == 'scissors' && computerSelection.toLowerCase() == 'rock'){
-        
-        console.log("You lost");
-
-    }else{
-        console.log("You win!");
-    }
+game();
+game();
+game();
+game();
+game();
+if (playerScore > computerScore){
+    console.log("You are the winner of the match!");
+}else if (computerScore > playerScore){
+    console.log("You lost that one! Better luck next time");
+}else{
+    console.log("How did this happen? It's a draw");
 }
-playRound(playerSelection, computerSelection);
